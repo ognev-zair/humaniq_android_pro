ccc85a6177619fc9965fbe794bd8a6440c8f180d
ognev <zair407@gmail.com>
2017-07-21 16:14:02 +0500

    Adding extentions

--- app/src/main/java/pro/humaniq/com/MainActivity.kt
+++ app/src/main/java/pro/humaniq/com/MainActivity.kt
@@ -2,6 +2,7 @@ package pro.humaniq.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.text
 import pro.humaniq.com.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    text.text = "hello HUMANIQ PRO"
   }
 }
