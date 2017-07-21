d9c2bfc944e61a9ab7a29b4903315cf09e0eae23
Alex Gritsay <aleksandr.gritsay@gmail.com>
2017-07-21 17:12:42 +0600

    package name

--- app/src/main/java/pro/humaniq/com/MainActivity.kt
+++ app/src/main/java/pro/humaniq/com/MainActivity.kt
@@ -1,14 +1,13 @@
-package pro.humaniq.com.humaniqpro
+package pro.humaniq.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.text
+import pro.humaniq.com.R
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    text.text = "hello HUMANIQ PRO"
   }
 }
